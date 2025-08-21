9d422688738feb2df7528180634f7e5fa59431d8
Eugene Taabazuing <eutaaba@gmail.com>
2025-08-21 13:12:26 -0600

    Delete validators directory

--- validators/base.py
+++
@@ -1,9 +0,0 @@
-
-from abc import ABC, abstractmethod
-from typing import List
-from core.models import ValidationIssue   
-
-class BaseValidator(ABC):
-    @abstractmethod
-    def validate(self, file_path: str) -> List[ValidationIssue]:
-        raise NotImplementedError
